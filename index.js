0cb2e64e988c196eb86bfbc05b494f943fb147a1
Ayon <ayonjodder177@gmail.com>
2022-05-07 12:34:16 +0600

    JWT server side done

--- index.js
+++ index.js
@@ -18,6 +18,28 @@ app.get('/', (req, res) => {
 const uri = `mongodb+srv://assignment:zTT4VLCVfmAEAWVO@cluster0.1mu84.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 async function run() {
+
+    function CheckJWTToken(req, res, next) {
+        const hederAuth = req.headers.authorization
+        if (!hederAuth) {
+            return res.status(401).send({ message: 'unauthorized access.try again' })
+        }
+        else {
+            const token = hederAuth.split(' ')[1]
+            console.log({ token });
+            jwt.verify(token, process.env.TOKEN, (err, decoded) => {
+                if (err) {
+                    console.log(err);
+                    return res.status(403).send({ message: 'forbidden access' })
+                }
+                console.log('decoded', decoded);
+                req.decoded = decoded;
+                next()
+            })
+        }
+        console.log(hederAuth, 'inside chckjwt');
+
+    }
     try {
         await client.connect();
         const collection = client.db("assignment").collection("product");
@@ -93,6 +115,34 @@ async function run() {
             res.send(result);
         })
 
+        //JWT
+        app.post('/signin', async (req, res) => {
+            const user = req.body;
+            console.log(req.body, 'user')
+
+            const getToken = jwt.sign(user, process.env.TOKEN, {
+                expiresIn: '1d'
+            });
+
+            res.send({ getToken });
+
+        })
+
+        // get items by email 
+        app.get('/singleItem', CheckJWTToken, async (req, res) => {
+            const decodedEmail = req.decoded.email
+            const email = req.query.email
+            if (email === decodedEmail) {
+                const query = { email: email }
+                const cursor = collection.find(query)
+                const items = await cursor.toArray()
+                res.send(items)
+            }
+            else {
+                return res.status(403).send({ message: 'forbidden access' })
+            }
+        })
+
     }
     finally {
         // await client.close()
@@ -107,5 +157,3 @@ run().catch(console.dir)
 app.listen(port, () => {
     console.log('Listening optional callback')
 })
-
-
